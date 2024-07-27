49f60856eecc7eb628bbcbff90e7243e471508bf
Ritik6559 <ritikjoshi741@gmail.com>
2024-07-27 14:00:29 +0530

    adding stats screen

--- lib/features/stats/screen/statistics_screen.dart
+++ lib/features/stats/screen/statistics_screen.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
 
-class StatisticsScreen extends StatelessWidget {
+class StatisticsScreen extends StatefulWidget {
   const StatisticsScreen({super.key});
 
   @override
+  State<StatisticsScreen> createState() => _StatisticsScreenState();
+}
+
+class _StatisticsScreenState extends State<StatisticsScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  List<String> tabs = [
+    'Weekly',
+    'Monthly',
+    'Yearly',
+    'All',
+  ];
+
+  @override
+  void initState() {
+    _tabController = TabController(
+      length: tabs.length,
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Statistics",
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 25,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          TabBar(
+            controller: _tabController,
+            tabs: tabs
+                .map(
+                  (e) => Text(e),
+                )
+                .toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
